ffa6016a90dc12f5aa4875805789ae48cdffa4d1
Martin Owens <doctormo@gmail.com>
2016-05-10 18:10:56 -0400

    Add new base test class module migrated code from inkscape

--- autotest/__init__.py
+++ autotest/__init__.py
@@ -3,4 +3,4 @@ Django command for quickly re-running tests when ever a file changes.
 """
 default_app_config = 'autotest.app.AutotestConfig'
 __pkgname__ = 'django-autotest-command'
-__version__ = '1.4.1'
+__version__ = '1.5'
